3fcd7b63d0bf195520811f0dac1eb1d6a1b83c70
GAMELOFT\sai.prg-training <sai.prg-training@gameloft.com>
2019-01-07 16:30:30 +0700

    update stateMachine

--- Project/AngryBear/AngryBear/Model/GameState/StateMachine.h
+++ Project/AngryBear/AngryBear/Model/GameState/StateMachine.h
@@ -17,6 +17,47 @@ private:
 
 	void AddState(State* newState, bool isReplacing = true);
 	void RemoveState();
-	void ProcessStateChanges();
+	void SwitchState();
 	State* &GetActiveState();
 }; 
+
+void StateMachine::AddState(State* newState, bool isReplacing) {
+	this->m_isAdding = true;
+	this->m_isReplacing = isReplacing;
+	
+	this->m_newState = move(newState); //move all resource from newState to this->newState
+}
+
+void StateMachine::RemoveState() {
+	this->m_isRemoving = true;
+}
+
+void StateMachine::SwitchState() {
+	if (this->m_isRemoving && !this->m_states.empty()) {
+		this->m_states.pop();
+
+		if (!this->m_states.empty())
+		{
+			this->m_states.top()->Resume();
+		}
+		this->m_isRemoving = false;
+	}
+
+	if (this->m_isAdding) {
+		if (!this->m_states.empty())
+		{
+			this->m_states.pop();
+		}
+		else {
+			this->m_states.top()->Pause();
+		}
+
+		this->m_states.push(move(this->m_newState));
+		this->m_states.top()->Init();
+		this->m_isAdding = false;
+	}
+}
+
+State* &StateMachine::GetActiveState() {
+	return this->m_states.top();
+}
